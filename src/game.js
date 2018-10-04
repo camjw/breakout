2e9fdc9bd6ae4a1655cdfd5fef49ad8949d0ed90
Cameron Whitehead <camjw119@gmail.com>
2018-10-04 19:50:22 +0100

    Updated collision checking

--- src/game.js
+++ src/game.js
@@ -19,14 +19,38 @@ Game.prototype.collisionDetection = function() {
       if (this.ball.position.x > this.bricks.brickArray[r][c].x &&
         this.ball.position.x < this.bricks.brickArray[r][c].x + this.bricks.BRICK_SIZE.width &&
         this.ball.position.y > this.bricks.brickArray[r][c].y &&
-        this.ball.position.y < this.bricks.brickArray[r][c].y + this.bricks.BRICK_SIZE.height &&
+        this.ball.position.y - this.ball.RADIUS - this.ball.velocity.y < this.bricks.brickArray[r][c].y + this.bricks.BRICK_SIZE.height &&
         this.bricks.brickArray[r][c].display) {
           this.ball.velocity.y = -this.ball.velocity.y;
           this.bricks.brickArray[r][c].display = false;
-        }
+      }
+      if (this.ball.position.x > this.bricks.brickArray[r][c].x &&
+        this.ball.position.x < this.bricks.brickArray[r][c].x + this.bricks.BRICK_SIZE.width &&
+        this.ball.position.y < this.bricks.brickArray[r][c].y &&
+        this.ball.position.y + this.ball.RADIUS + this.ball.velocity.y > this.bricks.brickArray[r][c].y + this.bricks.BRICK_SIZE.height &&
+        this.bricks.brickArray[r][c].display) {
+          this.ball.velocity.y = -this.ball.velocity.y;
+          this.bricks.brickArray[r][c].display = false;
+      }
+      if (this.ball.position.y > this.bricks.brickArray[r][c].y &&
+        this.ball.position.y < this.bricks.brickArray[r][c].y + this.bricks.BRICK_SIZE.height &&
+        this.ball.position.x > this.bricks.brickArray[r][c].x &&
+        this.ball.position.x - this.ball.RADIUS + this.ball.velocity.x < this.bricks.brickArray[r][c].x + this.bricks.BRICK_SIZE.width &&
+        this.bricks.brickArray[r][c].display) {
+          this.ball.velocity.x = -this.ball.velocity.x;
+          this.bricks.brickArray[r][c].display = false;
+      }
+      if (this.ball.position.y > this.bricks.brickArray[r][c].y &&
+        this.ball.position.y < this.bricks.brickArray[r][c].y + this.bricks.BRICK_SIZE.height &&
+        this.ball.position.x < this.bricks.brickArray[r][c].x &&
+        this.ball.position.x + this.ball.RADIUS - this.ball.velocity.x > this.bricks.brickArray[r][c].x + this.bricks.BRICK_SIZE.width &&
+        this.bricks.brickArray[r][c].display) {
+          this.ball.velocity.x = -this.ball.velocity.x;
+          this.bricks.brickArray[r][c].display = false;
       }
     }
   }
+}
 
 Game.prototype.run = function() {
 
